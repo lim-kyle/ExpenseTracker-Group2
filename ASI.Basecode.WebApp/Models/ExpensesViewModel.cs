b43b034440e714e6539ae50427a81f0bbac16dde
xaenic <allanvillegas35@gmail.com>
2024-11-10 06:55:58 +0800

    hehe

---
+++ ASI.Basecode.WebApp/Models/ExpensesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ASI.Basecode.WebApp.Models
+{
+    /// <summary>
+    /// Login View Model
+    /// </summary>
+    public class ExpensesViewModel
+    {
+        /// <summary>ユーザーID</summary>
+        [JsonPropertyName("username")]
+        [Required(ErrorMessage = "UserId is required.")]
+        public string Username { get; set; }
+        /// <summary>パスワード</summary>
+        [JsonPropertyName("password")]
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+}
