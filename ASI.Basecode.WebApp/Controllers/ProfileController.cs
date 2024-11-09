d3e380f5345f02bb16ed4cc924c9c0d36cb1ec5b
xaenic <allanvillegas35@gmail.com>
2024-11-10 06:55:50 +0800

    Add ProfileController with user profile retrieval
    Introduced a new `ProfileController` in the `ASI.Basecode.WebApp.Controllers` namespace.
    Injected `IUserService` via constructor for dependency management.
    Implemented `Index` action to fetch user details from claims and return a view.
    Returns `NotFound` if the user is not found.

---
+++ ASI.Basecode.WebApp/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using ASI.Basecode.Data.Models;
+using ASI.Basecode.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IUserService _userService;
+        public ProfileController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+        public IActionResult Index()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "UserId");
+            var user = _userService.GetUser(int.Parse(userIdClaim.Value));
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+    }
+}
