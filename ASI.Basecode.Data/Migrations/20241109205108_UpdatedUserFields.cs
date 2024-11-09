f8128eb149cb3be46a89b370f6da260c454a67ee
xaenic <allanvillegas35@gmail.com>
2024-11-10 05:07:41 +0800

    Update Users table: rename Name, add Email & FirstName
    Introduce migration `20241109205108_UpdatedUserFields`:
    - Rename `Name` column to `LastName` in `Users` table.
    - Add `Email` and `FirstName` columns (varchar(50), non-nullable).
    - Auto-generate `20241109205108_UpdatedUserFields.Designer.cs`.
    - Update `AsiBasecodeDBContextModelSnapshot.cs` to reflect changes.

---
+++ ASI.Basecode.Data/Migrations/20241109205108_UpdatedUserFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASI.Basecode.Data.Migrations
+{
+    public partial class UpdatedUserFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Name");
+        }
+    }
+}
