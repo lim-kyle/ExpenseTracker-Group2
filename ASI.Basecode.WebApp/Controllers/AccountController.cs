954e36be446b10778e47843c2b1d5dd7bc466a38
xaenic <allanvillegas35@gmail.com>
2024-11-22 20:29:52 +0800

    Remove unnecessary whitespace in AccountController

--- ASI.Basecode.WebApp/Controllers/AccountController.cs
+++ ASI.Basecode.WebApp/Controllers/AccountController.cs
@@ -144,5 +144,7 @@ namespace ASI.Basecode.WebApp.Controllers
             await this._signInManager.SignOutAsync();
             return RedirectToAction("Login", "Account");
         }
+
+      
     }
 }
