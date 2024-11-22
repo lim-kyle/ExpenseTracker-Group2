61517e13a05c5203667d37b4663dc4172df69f98
xaenic <allanvillegas35@gmail.com>
2024-11-22 20:29:03 +0800

    Enhance User model with validation attributes for required fields

--- ASI.Basecode.Data/Models/User.cs
+++ ASI.Basecode.Data/Models/User.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASI.Basecode.Data.Models
 {
     public partial class User
     {
         public int Id { get; set; }
+
+        [Required]
         public string Username { get; set; }
+        [Required]
+
         public string FirstName { get; set; }
+        [Required]
+
         public string LastName { get; set; }
+        [Required]
+
         public string Email { get; set; }
+        [Required]
+
         public string Password { get; set; }
+
         public string CreatedBy { get; set; }
         public DateTime CreatedTime { get; set; }
         public string UpdatedBy { get; set; }
