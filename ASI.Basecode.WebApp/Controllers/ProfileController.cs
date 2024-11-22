ede252076c29775d0bff6c7fbde54923c793d7cc
xaenic <allanvillegas35@gmail.com>
2024-11-22 20:30:09 +0800

    Add profile update functionality in ProfileController with password validation

--- ASI.Basecode.WebApp/Controllers/ProfileController.cs
+++ ASI.Basecode.WebApp/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using ASI.Basecode.Data.Models;
 using ASI.Basecode.Services.Interfaces;
+using ASI.Basecode.Services.Manager;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
@@ -21,6 +22,46 @@ namespace ASI.Basecode.WebApp.Controllers
                 return NotFound();
             }
             return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user, string NewPassword, string ConfirmPassword)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "UserId");
+                var existingUser = _userService.GetUser(int.Parse(userIdClaim.Value));
+               if(existingUser == null)
+               {
+                     return NotFound();
+               }
+               if(existingUser.Password != PasswordManager.EncryptPassword(user.Password))
+               {
+                   
+                   TempData["ErrorMessage"] = "Password is incorrect";
+                    return View(user);
+               }
+
+               if(!string.IsNullOrEmpty(NewPassword)) {
+                if(NewPassword == ConfirmPassword)
+                {
+                    existingUser.Password = PasswordManager.EncryptPassword(NewPassword);
+                }else {
+                        TempData["ErrorMessage"] = "Password and Confirm Password do not match";
+                        return View(user);
+                }
+               }
+               
+                existingUser.FirstName = user.FirstName;
+                existingUser.LastName = user.LastName;
+                existingUser.Email = user.Email;
+                _userService.UpdateUser(existingUser);
+              TempData["SuccessMessage"] = "Profile updated successfully";
+              return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
