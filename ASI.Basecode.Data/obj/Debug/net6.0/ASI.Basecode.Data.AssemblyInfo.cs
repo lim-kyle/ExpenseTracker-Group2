d059ff50d9acbd63e3e876244394bcb63e5f7ca7
xaenic <allanvillegas35@gmail.com>
2024-12-04 00:09:15 +0800

    ok

--- ASI.Basecode.Data/obj/Debug/net6.0/ASI.Basecode.Data.AssemblyInfo.cs
+++ ASI.Basecode.Data/obj/Debug/net6.0/ASI.Basecode.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASI.Basecode.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef9dd2d923b55bd8db0309f3c66982c19d174fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9522fe6d94f231880aeb2f6ebb1474354506cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASI.Basecode.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASI.Basecode.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
